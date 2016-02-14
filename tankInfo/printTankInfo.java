a5bd7487b0991eab427f4265a1749a9c6e4738d8
Lintoshy <lintoshy@gmail.com>
2016-02-14 20:01:59 +0200

    Second commit

--- tankInfo/printTankInfo.java
+++ tankInfo/printTankInfo.java
@@ -3,12 +3,17 @@ package tankInfo;
 public class printTankInfo {
 
 	public static void main(String[] args) {
-		printTankInfo("red", 2, 120);
-
+		Tank destroyer = new Tank();
+		destroyer.color = "red";
+		destroyer.crew = 2;
+		destroyer.maxSpeed = 250;
+		
+		printTankInfo(destroyer);
 	}
-	public static void printTankInfo(String color, int crew, int maxSpeed) {
+	
+	public static void printTankInfo(Tank tank) {
 		System.out.print("Tank properties: ");
-		System.out.print("color " + color + ", crew " + crew + ", maxSpeed " + maxSpeed);
+		System.out.print("color " + tank.color + ", crew " + tank.crew + ", maxSpeed " + tank.maxSpeed);
 		
 	}
 
