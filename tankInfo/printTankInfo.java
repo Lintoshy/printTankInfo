5a74f1633c2095a3f6fd0a92591b3f525502d74c
Lintoshy <lintoshy@gmail.com>
2016-02-14 20:48:21 +0200

    Third

--- tankInfo/printTankInfo.java
+++ tankInfo/printTankInfo.java
@@ -7,20 +7,12 @@ public class printTankInfo {
 		destroyer.color = "red";
 		destroyer.crew = 2;
 		destroyer.maxSpeed = 90;
-		destroyer.name = "destroyer";
-		
-		Tank T90 = new Tank();
-		T90.color = "khaki";
-		T90.crew = 3;
-		T90.maxSpeed = 65;
-		T90.name = "T90";
-		
+			
 		printTankInfo(destroyer);
-		printTankInfo(T90);
 	}
 	
 	public static void printTankInfo(Tank tank) {
-		System.out.print("Tank " + tank.name + " properties: ");
+		System.out.print("Tank properties: ");
 		System.out.println("color " + tank.color + ", crew " + tank.crew + ", maxSpeed " + tank.maxSpeed);
 	}
 
